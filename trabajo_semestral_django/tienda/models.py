b95f8d69d64b96a1073e25a615c5ee9bdd80e116
Jearamo <jeh.arancibia@duocuc.cl>
2024-07-01 13:50:48 -0400

    ENTORNO VIRTUAL Y PILLOW

--- trabajo_semestral_django/tienda/models.py
+++ trabajo_semestral_django/tienda/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to='productos/')
-    stock = models.IntegerField(default=0)  # Nuevo campo de stock
+    stock = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.imagen.path)
+
+        if img.height > 800 or img.width > 800:
+            output_size = (800, 800)
+            img.thumbnail(output_size)
+            img.save(self.imagen.path)
 
     def __str__(self):
         return self.nombre
+
 
 class Carrito(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
